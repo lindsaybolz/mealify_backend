e978899f42fe308e3b840e04072d6a2013d5b200
Lindsay <lindsaybolzwilson@gmail.com>
2023-07-27 15:32:40 -0600

    First draft of Recipe and Pantry App.

--- mealify_backend/rest_api/models.py
+++ mealify_backend/rest_api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
+class Pantry(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    food_list = models.JSONField()
+    updated = models.DataTimeField('Last Updated On')
+    staples = models.JSONField()
+
+class Recipe(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    ingredients = models.JSONField()
+    instructions = models.JSONField()
+    nutritional_data = models.JSONField()
+    url = models.URLField(max_length=200)
+    user_state = models.IntegerField(validators=[MinValueValidator(-1), MaxValueValidator(1)])
