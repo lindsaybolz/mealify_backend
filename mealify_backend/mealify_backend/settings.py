98545a837ccaf61b05d1f705db1dfc46cb5db83e
Lindsay <lindsaybolzwilson@gmail.com>
2023-07-27 16:01:30 -0600

    Added custom user model.

--- mealify_backend/mealify_backend/settings.py
+++ mealify_backend/mealify_backend/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_api.apps.RestApiConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_api',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "rest_api.User"
--- mealify_backend/rest_api/models.py
+++ mealify_backend/rest_api/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
-
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    alergies = models.JSONField()
+    restrictions = models.JSONField()
+    prefrences = models.JSONField()
+
+
 class Pantry(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     food_list = models.JSONField()
     updated = models.DataTimeField('Last Updated On')
     staples = models.JSONField()
+
 
 class Recipe(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
