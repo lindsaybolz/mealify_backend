17fbc48f7c2d913fecaa26afdd9df6426bffb3fc
Lindsay <lindsaybolzwilson@gmail.com>
2023-07-28 11:29:53 -0600

    added some login and logout views that are very unfinished

--- mealify_backend/rest_api/views.py
+++ mealify_backend/rest_api/views.py
@@ -1,6 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+from django.contrib.auth import authenticate, login, logout
+
+from .models import User, Pantry, Recipe
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World")
+
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = "TEST"
+    # user = authenticate(request, username=username, password=password)
+    if user is not None:
+        # login(request, user)
+        HttpResponse(f'User {username} successfully loged in.')
+        # response
+
+    else:
+        # invalid response
+        pass
+
+
+def logout_view(request):
+    logout(request)
+
+class UserListView():
+    model = User
+    def get(self, request):
+        pass
+
+
+    def delete(self, request):
+        pass
+
+
+    def post(self, request):
+        pass
+
+
+    def patch(self, request):
+        pass
